5b64433630bd5d83e6d785a8d0f0c618faf6e9f1
INMD1 <lyw514549@gmail.com>
2021-09-03 00:09:11 +0900

    fix

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const fs = require('fs');
+const path = require('path');
 const puppeteer = require('puppeteer');
+
+const isPkg = typeof process.pkg !== 'undefined';
+
+console.log(process.platform)
+//check win32
+if (process.platform == 'win32') {
+  chromiumExecutablePath = (isPkg ?
+    puppeteer.executablePath().replace(
+      /^.*?\\node_modules\\puppeteer\\\.local-chromium/,
+      path.join(path.dirname(process.execPath), 'chromium')
+    ) :
+    puppeteer.executablePath()
+  );
+}
+
 var dataFile;
 try {
      dataFile = fs.readFileSync('Scret.json'); 
@@ -18,9 +34,10 @@ console.log('------------------------------------');
 console.log('기숙사 자동 발열체크 프로그램 v0.0.1');
 console.log('------------------------------------');
 console.log('json 분석결과 ' + count + '명을 발견했습니다. ' + time);
+
 (async() => {
     const browser = await puppeteer.launch({
-      headless: false
+      executablePath: chromiumExecutablePath,headless: false ,
     });
     for (var i = 0; i < count; i++) {
     console.log('[pending] ID:'+ data[i].id + '하는중... ' + time);
@@ -52,16 +69,20 @@ console.log('json 분석결과 ' + count + '명을 발견했습니다. ' + time)
       await page.waitForNavigation( ),
       await page.type("#mG61Hd > div.freebirdFormviewerViewFormCard.exportFormCard > div > div.freebirdFormviewerViewItemList > div:nth-child(1) > div > div > div.freebirdFormviewerComponentsQuestionTextRoot > div > div.quantumWizTextinputPaperinputMainContent.exportContent > div > div.quantumWizTextinputPaperinputInputArea > input", data[i].name),
       await page.type("#mG61Hd > div.freebirdFormviewerViewFormCard.exportFormCard > div > div.freebirdFormviewerViewItemList > div:nth-child(2) > div > div > div.freebirdFormviewerComponentsQuestionTextRoot > div > div.quantumWizTextinputPaperinputMainContent.exportContent > div > div.quantumWizTextinputPaperinputInputArea > input", data[i].id),
-      await page.type("#mG61Hd > div.freebirdFormviewerViewFormCard.exportFormCard > div > div.freebirdFormviewerViewItemList > div:nth-child(2) > div > div > div.freebirdFormviewerComponentsQuestionTextRoot > div > div.quantumWizTextinputPaperinputMainContent.exportContent > div > div.quantumWizTextinputPaperinputInputArea > input", data[i].room),
-      await page.type("#mG61Hd > div.freebirdFormviewerViewFormCard.exportFormCard > div > div.freebirdFormviewerViewItemList > div:nth-child(2) > div > div > div.freebirdFormviewerComponentsQuestionTextRoot > div > div.quantumWizTextinputPaperinputMainContent.exportContent > div > div.quantumWizTextinputPaperinputInputArea > input", String(max +'.'+rand(0, 8))),
+      await page.type("#mG61Hd > div.freebirdFormviewerViewFormCard.exportFormCard > div > div.freebirdFormviewerViewItemList > div:nth-child(3) > div > div > div.freebirdFormviewerComponentsQuestionTextRoot > div > div.quantumWizTextinputPaperinputMainContent.exportContent > div > div.quantumWizTextinputPaperinputInputArea > input", data[i].room),
+      await page.type("#mG61Hd > div.freebirdFormviewerViewFormCard.exportFormCard > div > div.freebirdFormviewerViewItemList > div:nth-child(4) > div > div > div.freebirdFormviewerComponentsQuestionTextRoot > div > div.quantumWizTextinputPaperinputMainContent.exportContent > div > div.quantumWizTextinputPaperinputInputArea > input", String(max +'.'+rand(0, 8))),
       await page.click("#i21"),
-      await page.click("#mG61Hd > div.freebirdFormviewerViewFormCard.exportFormCard > div > div.freebirdFormviewerViewNavigationNavControls > div.freebirdFormviewerViewNavigationButtonsAndProgress > div > div > span > span");  
+      await page.click("#mG61Hd > div.freebirdFormviewerViewFormCard.exportFormCard > div > div.freebirdFormviewerViewNavigationNavControls > div.freebirdFormviewerViewNavigationButtonsAndProgress > div > div > span");  
       await page.close();
     }
       console.log('[SUCCESS] ID:'+ data[i].id + ' 정상적으로 됨... ' + time);
     }
     await browser.close();
+    console.log('성공적으로 작업을 전부 완료 했습니다!');
+    console.log('종료할려면 아무키나 눌려주세요.');
   })();
 
-
+  process.stdin.setRawMode(true);
+  process.stdin.resume();
+  process.stdin.on('data', process.exit.bind(process, 0));
 
